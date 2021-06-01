f7b2aa50385135db1c822f449ec13c2dc124db58
An Jihyun <hwiyu25@gmail.com>
2021-06-01 17:44:58 +0900

    ✨ theme 생성 및 ThemeProvider 적용

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
+import React from "react";
+import { ThemeProvider } from "styled-components";
 import GlobalFonts from "./components/styles/GlobalFonts";
 import Normalize from "./components/styles/Normalize";
 import GlobalReset from "./components/styles/GlobalReset";
 import Router from "./Router";
 import "./styles/main.scss";
+import theme from "./components/styles/theme";
 
 const App = () => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalFonts />
       <Normalize />
       <GlobalReset />
       <Router />
-    </>
+    </ThemeProvider>
   );
 };
 
---
+++ src/components/styles/theme.js
@@ -0,0 +1,51 @@
+const mainFont = "NEXON Lv2 Gothic";
+
+const colors = {
+  black: "#000000d",
+  white: "#ffffff",
+  ivory: "#eeede7",
+  pink: "#e7d2cc",
+  brown: "#b67768",
+  lightGray: "#b9b7bd",
+  darkGray: "#868b8e",
+};
+
+const typography = {
+  fontSize12: "12px",
+  lineHeight12: "18px",
+  letterSpacing12: "0",
+
+  fontSize14: "14px",
+  lineHeight14: "20px",
+  letterSpacing14: "0.01em",
+
+  fontSize16: "16px",
+  lineHeight16: "24px",
+  letterSpacing16: "0.02em",
+
+  fontSize20: "20px",
+  lineHeight20: "28px",
+  letterSpacing20: "0.02em",
+
+  fontSize24: "24px",
+  lineHeight24: "36px",
+  letterSpacing24: "0.02em",
+
+  fontSize34: "34px",
+  lineHeight34: "50px",
+  letterSpacing34: "0.02em",
+};
+
+const padding = {
+  smPadding: "20px",
+  mdPadding: "40px",
+};
+
+const theme = {
+  mainFont,
+  colors,
+  typography,
+  padding,
+};
+
+export default theme;

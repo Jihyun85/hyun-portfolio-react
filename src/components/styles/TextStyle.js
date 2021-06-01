611265d34117d221548e285f1fe746ac63e8fe07
An Jihyun <hwiyu25@gmail.com>
2021-06-01 17:45:16 +0900

    ✨ TestStyle mixin 생성(styled-components)

---
+++ src/components/styles/TextStyle.js
@@ -0,0 +1,37 @@
+import { css } from "styled-components";
+
+export const textStyle12 = css`
+  font-size: ${({ theme }) => theme.typography.fontSize12};
+  line-height: ${({ theme }) => theme.typography.lineHeight12};
+  letter-spacing: ${({ theme }) => theme.typography.letterSpacing12};
+`;
+
+export const textStyle14 = css`
+  font-size: ${({ theme }) => theme.typography.fontSize14};
+  line-height: ${({ theme }) => theme.typography.lineHeight14};
+  letter-spacing: ${({ theme }) => theme.typography.letterSpacing14};
+`;
+
+export const textStyle16 = css`
+  font-size: ${({ theme }) => theme.typography.fontSize16};
+  line-height: ${({ theme }) => theme.typography.lineHeight16};
+  letter-spacing: ${({ theme }) => theme.typography.letterSpacing16};
+`;
+
+export const textStyle20 = css`
+  font-size: ${({ theme }) => theme.typography.fontSize20};
+  line-height: ${({ theme }) => theme.typography.lineHeight20};
+  letter-spacing: ${({ theme }) => theme.typography.letterSpacing20};
+`;
+
+export const textStyle24 = css`
+  font-size: ${({ theme }) => theme.typography.fontSize24};
+  line-height: ${({ theme }) => theme.typography.lineHeight24};
+  letter-spacing: ${({ theme }) => theme.typography.letterSpacing24};
+`;
+
+export const textStyle34 = css`
+  font-size: ${({ theme }) => theme.typography.fontSize34};
+  line-height: ${({ theme }) => theme.typography.lineHeight34};
+  letter-spacing: ${({ theme }) => theme.typography.letterSpacing34};
+`;

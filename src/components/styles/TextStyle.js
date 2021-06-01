d5453415a8ce14890ffe0ed1ddd4a380c28d5bda
An Jihyun <hwiyu25@gmail.com>
2021-06-01 18:25:47 +0900

    🩹 TextStyle 컴포넌트 이름변경 (textStyle → TextStyle)

--- src/components/styles/TextStyle.js
+++ src/components/styles/TextStyle.js
@@ -1,36 +1,36 @@
 import { css } from "styled-components";
 
-export const textStyle12 = css`
+export const TextStyle12 = css`
   font-size: ${({ theme }) => theme.typography.fontSize12};
   line-height: ${({ theme }) => theme.typography.lineHeight12};
   letter-spacing: ${({ theme }) => theme.typography.letterSpacing12};
 `;
 
-export const textStyle14 = css`
+export const TextStyle14 = css`
   font-size: ${({ theme }) => theme.typography.fontSize14};
   line-height: ${({ theme }) => theme.typography.lineHeight14};
   letter-spacing: ${({ theme }) => theme.typography.letterSpacing14};
 `;
 
-export const textStyle16 = css`
+export const TextStyle16 = css`
   font-size: ${({ theme }) => theme.typography.fontSize16};
   line-height: ${({ theme }) => theme.typography.lineHeight16};
   letter-spacing: ${({ theme }) => theme.typography.letterSpacing16};
 `;
 
-export const textStyle20 = css`
+export const TextStyle20 = css`
   font-size: ${({ theme }) => theme.typography.fontSize20};
   line-height: ${({ theme }) => theme.typography.lineHeight20};
   letter-spacing: ${({ theme }) => theme.typography.letterSpacing20};
 `;
 
-export const textStyle24 = css`
+export const TextStyle24 = css`
   font-size: ${({ theme }) => theme.typography.fontSize24};
   line-height: ${({ theme }) => theme.typography.lineHeight24};
   letter-spacing: ${({ theme }) => theme.typography.letterSpacing24};
 `;
 
-export const textStyle34 = css`
+export const TextStyle34 = css`
   font-size: ${({ theme }) => theme.typography.fontSize34};
   line-height: ${({ theme }) => theme.typography.lineHeight34};
   letter-spacing: ${({ theme }) => theme.typography.letterSpacing34};

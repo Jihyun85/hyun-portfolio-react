59b562ad635a943573489487f13cc80f25af1ee9
An Jihyun <hwiyu25@gmail.com>
2021-06-01 15:17:52 +0900

    🎨 Normalize.scss를 Nomalize GlobalStyle 컴포넌트로 변경

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import Normalize from "./components/styles/Normalize";
 import Router from "./Router";
 import "./styles/main.scss";
 
 const App = () => {
-  return <Router></Router>;
+  return (
+    <>
+      <Normalize />
+      <Router />
+    </>
+  );
 };
 
 export default App;

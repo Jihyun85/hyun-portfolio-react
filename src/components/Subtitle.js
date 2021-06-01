d203741a2e374aa80180e7d2bbd5578be8af69af
An Jihyun <hwiyu25@gmail.com>
2021-06-01 13:19:38 +0900

    ✨ Subtitle 컴포넌트 생성

---
+++ src/components/Subtitle.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Subtitle = ({ subtitle }) => (
+  <div className="subtitle">
+    <h2 class="subtitle-text">{subtitle}</h2>
+    <div class="divider"></div>
+  </div>
+);
+
+export default Subtitle;
